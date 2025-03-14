a27379faf962bb9893092956d09cb1529e3b696a
Renzo Aimaretti <renzoceronueve@gmail.com>
2025-03-14 09:48:55 -0300

    uso de variables de entorno para la conexion a bd

--- login.php
+++ login.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "veterinaria";
-
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 // Crear conexión
-$conn = new mysqli("localhost", "root", "", "veterinaria");
+$conn = new mysqli($_ENV['servername'], $_ENV['username'], $_ENV['password'], $_ENV['dbname']);
 
 if ($conn->connect_error) {
   die("Error de conexión: " . $conn->connect_error);
