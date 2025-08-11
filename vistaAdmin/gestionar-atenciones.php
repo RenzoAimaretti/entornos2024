cc3364a7b98c1e187acd6ce5104967c94bdfd117
Renzo Aimaretti <renzoceronueve@gmail.com>
2025-08-11 16:31:57 -0300

    rangos fecha hardcodeado

--- vistaAdmin/gestionar-atenciones.php
+++ vistaAdmin/gestionar-atenciones.php
@@ -59,7 +59,40 @@ session_start();
                             </div>
                             <div class="form-group">
                                 <label for="hora">Hora</label>
-                                <input type="time" class="form-control" id="hora" name="hora" required>
+                                <select class="form-control" id="hora" name="hora" required>
+                                    <option value="" disabled selected>Seleccione hora</option>
+                                    <option value="09:00">09:00</option>
+                                    <option value="09:15">09:15</option>
+                                    <option value="09:30">09:30</option>
+                                    <option value="09:45">09:45</option>
+                                    <option value="10:00">10:00</option>
+                                    <option value="10:15">10:15</option>
+                                    <option value="10:30">10:30</option>
+                                    <option value="10:45">10:45</option>
+                                    <option value="11:00">11:00</option>
+                                    <option value="11:15">11:15</option>
+                                    <option value="11:30">11:30</option>
+                                    <option value="11:45">11:45</option>
+                                    <option value="12:00">12:00</option>
+                                    <option value="12:15">12:15</option>
+                                    <option value="12:30">12:30</option>
+                                    <option value="12:45">12:45</option>
+                                    <option value="13:00">13:00</option>
+                                    <option value="13:15">13:15</option>
+                                    <option value="13:30">13:30</option>
+                                    <option value="13:45">13:45</option>
+                                    <option value="14:00">14:00</option>
+                                    <option value="14:15">14:15</option>
+                                    <option value="14:30">14:30</option>
+                                    <option value="14:45">14:45</option>
+                                    <option value="15:00">15:00</option>
+                                    <option value="15:15">15:15</option>
+                                    <option value="15:30">15:30</option>
+                                    <option value="15:45">15:45</option>
+                                    <option value="16:00">16:00</option>
+                                    <option value="16:15">16:15</option>
+                                    <option value="16:30">16:30</option>
+                                </select>
                             </div>
 
                             <!-- Campo Mascota con Autocomplete -->
